84d001612648f41dd3b0bbf5859111fe10b1ac55
Erick <henrique_eryck@hotmail.com.br>
2019-09-07 16:08:45 -0300

    .

--- Exercicios II/Ex_21.py
+++ Exercicios II/Ex_21.py
@@ -1,11 +1,11 @@
 
-categoria = null
+
 idade = int (input('Digite a idade do nadador : \n'))
 
 if (idade >= 5 and idade <= 7):
     categoria = 'Infantil A'
 elif(idade == 8 and idade <= 10):
-    categoria = 'Infatil B'
+    categoria = 'Infantil B'
 elif(idade == 11 and idade <= 13):
     categoria = 'Juvenil A'
 elif(idade == 14 and idade <= 17):
